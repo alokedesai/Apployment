74ef5cb1bef955b13554f911b9aa363498a7f06a
Aloke Desai <Aloke@Alokes-MacBook-Pro.local>
2014-04-13 22:41:20 -0700

    added str methods to models :+1

--- apployment_site/models.py
+++ apployment_site/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-	username = models.IntegerField()
+	username = models.EmailField(max_length=200)
 	first_name = models.CharField(max_length=200)
 	last_name = models.CharField(max_length=200)
 	school = models.CharField(max_length=200)
@@ -12,21 +12,39 @@ class User(models.Model):
 	major = models.CharField(max_length=200)
 	# add file field for resume
 
+	def __str__(self):
+		return "%s -%s -%s" % (self.username, user.first_name, user.last_name)
+
 class Ratings(models.Model):
 	rated = models.ForeignKey(User, related_name="rated")
 	rater = models.ForeignKey(User, related_name="rater")
-	start = models.IntegerField()
+	rating = models.IntegerField()
+
+	def __str__(self):
+		return "rated: %s , rater: %s, rating: %s" % (self.rated.username, self.rater.username, str(self.rating))
 class Skill(models.Model):
 	skill = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.skill
 
 class hasSkill(models.Model):
 	user = models.ForeignKey(User)
 	skill = models.ForeignKey(Skill)
+
+	def __str__(self):
+		return "%s - %s" % (self.user.username, self.skill.skill)
 class Experience(models.Model):
 	title = models.CharField(max_length=200)
 	company = models.CharField(max_length=200)
 	description = models.CharField(max_length=200)
+
+	def __str__(self):
+		return "%s - %s" % (self.title, self.company)
  
 class hasExperience(models.Model):
 	user = models.ForeignKey(User)
-	Experience = models.ForeignKey(Experience)
+	experience = models.ForeignKey(Experience)
+
+	def __str__(self):
+		return "%s - %s" %(self.user.username, self.experience.title)
