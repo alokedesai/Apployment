9d16cbe1046fee10dfedcbf8fb6eeb8e6cb4da78
Aloke Desai <aloke.desai@gmail.com>
2014-04-14 09:56:10 -0700

    querying skills

--- apployment_site/views.py
+++ apployment_site/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
+from apployment_site.models import *
 
 def index(request):
 	return render(request, "apployment_site/index.html")
 
 def signup(request):
-	return render(request, "apployment_site/signup.html")
+	options = Skill.objects.all()
+	return render(request, "apployment_site/signup.html", {"options" : options})
