71e04fa98d4795b23519b4a80e41a7e86852d261
Aloke Desai <aloke.desai@gmail.com>
2014-04-13 23:47:18 -0700

    modeled index :poop:

--- apployment_site/views.py
+++ apployment_site/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("Hello world")
+	return render(request, "apployment_site/index.html")
